aba051310751bddbfc0e0cf6dce50fd3fb41c821
htntang <htntang@gmail.com>
2023-05-10 23:47:53 -0600

    started application

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <title>Youssef Triki</title>
+      <div class="sidebar">
+        <a class="active" href="#home">Home</a>
+      </div>
+
+      <div class="content">
+        welcome
+      </div>
+    </>
+  );
+}
+
+export default App;
